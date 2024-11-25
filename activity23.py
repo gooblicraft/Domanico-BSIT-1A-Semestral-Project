9f8a81c118719d19d8e389b6c257100be6433345
EDRIANE-PC <24crafter05@gmail.com>
2024-11-25 19:59:19 +0800

    initial commit

---
+++ activity23.py
@@ -0,0 +1,9 @@
+#Function using docstring
+
+def sum(num1, num2):
+    """This function adds the first number and second number returnig sum value"""
+    return num1 + num2
+
+print(f"The sum of the numbers are ---> {sum(23,24)}")
+
+#help(sum) #delete the '#' to execute the docstring
---
+++ activity24.py
@@ -0,0 +1,8 @@
+from activity23 import sum
+from code_challenge1 import asteris as ast
+
+sum(28,2) #the function sum
+
+#use help(sum) function to execute the docstring
+
+ast() #should be asteris but since I declare it as 'ast', it must be executed as ast()
